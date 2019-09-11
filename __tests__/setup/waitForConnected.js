04cd248bc4b8f600a0dc989e68728d15beecd934
William Wong <compulim@hotmail.com>
2019-09-11 14:54:21 -0700

    Use ConnectionStatus.Online

--- __tests__/setup/waitForConnected.js
+++ __tests__/setup/waitForConnected.js
@@ -1,9 +1,11 @@
+import { ConnectionStatus } from '../../src/directLine';
+
 import waitForObservable from './waitForObservable';
 
 export default async function waitForConnected(directLine) {
   const subscription = directLine.activity$.subscribe();
 
-  await waitForObservable(directLine.connectionStatus$, 2);
+  await waitForObservable(directLine.connectionStatus$, ConnectionStatus.Online);
 
   return subscription.unsubscribe.bind(subscription);
 }
