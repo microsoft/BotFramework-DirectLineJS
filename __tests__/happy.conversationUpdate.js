8d6180d8d0daad4c1e40dcfb3af769c5b505a09b
CK Kashyap <ckkashyap@gmail.com>
2019-09-23 17:23:14 -0700

    Added conversationUpdate test

---
+++ __tests__/happy.conversationUpdate.js
@@ -0,0 +1,60 @@
+import 'dotenv/config';
+
+import onErrorResumeNext from 'on-error-resume-next';
+
+import { timeouts } from './constants.json';
+import * as createDirectLine from './setup/createDirectLine';
+import waitForBotToRespond from './setup/waitForBotToRespond';
+import waitForConnected from './setup/waitForConnected';
+
+describe('Happy path', () => {
+  let unsubscribes;
+
+  beforeEach(() => unsubscribes = []);
+  afterEach(() => unsubscribes.forEach(fn => onErrorResumeNext(fn)));
+
+  describe('should receive the welcome message from bot', () => {
+    let directLine;
+
+    describe('using REST', () => {
+      beforeEach(() => jest.setTimeout(timeouts.rest));
+
+      test('with secret', async () => {
+        directLine = await createDirectLine.forREST({ token: false });
+      });
+
+      test('with token', async () => {
+        directLine = await createDirectLine.forREST({ token: true });
+      });
+    });
+
+    test('using Streaming Extensions', async () => {
+      jest.setTimeout(timeouts.webSocket);
+      directLine = await createDirectLine.forStreamingExtensions();
+    });
+
+    describe('using Web Socket', () => {
+      beforeEach(() => jest.setTimeout(timeouts.webSocket));
+
+      test('with secret', async () => {
+        directLine = await createDirectLine.forWebSocket({ token: false });
+      });
+
+      test('with token', async () => {
+        directLine = await createDirectLine.forWebSocket({ token: false });
+      });
+    });
+
+    afterEach(async () => {
+      // If directLine object is undefined, that means the test is failing.
+      if (!directLine) { return; }
+
+      unsubscribes.push(directLine.end.bind(directLine));
+      unsubscribes.push(await waitForConnected(directLine));
+
+      await Promise.all([
+        waitForBotToRespond(directLine, ({ text }) => text === 'Welcome')
+      ]);
+    });
+  });
+});
