696d966ba064e1b12703a1bf6f259f79961120f8
William Wong <compulim@hotmail.com>
2019-09-26 10:00:39 -0700

    Formatting

--- src/dedupeFilenames.ts
+++ src/dedupeFilenames.ts
@@ -4,9 +4,9 @@ export default function dedupeFilenames(array: string[]) {
     const nextArray: string[] = [];
 
     array.forEach(value => {
+        const { extname, name } = parseFilename(value);
         let count = 0;
         let nextValue = value;
-		const { extname, name } = parseFilename(value);
 
         while (nextArray.includes(nextValue)) {
             nextValue = [name, `(${ (++count) })`].filter(segment => segment).join(' ') + extname;
