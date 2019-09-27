039de4198012453d5c4ef83c7a0076f284d5687b
Orgad Shaneh <orgads@gmail.com>
2019-09-28 00:53:35 +0300

    Avoid posting an error on intentional end (#172)
    Commit 544e7e328943f98d3ef2aa14404193ca08ab225f suppressed the exception,
    but it is still reported as an error.
    
    directLine.activity$
    .filter(activity => activity.type === 'message')
    .subscribe(
        message => {
          console.log("received message ", message)
          directLine.end();
        },
        err => console.error // error is logged here
    );

--- src/directLine.ts
+++ src/directLine.ts
@@ -410,6 +410,8 @@ export class DirectLine implements IBotConnection {
 
     private tokenRefreshSubscription: Subscription;
 
+    private ending: boolean;
+
     constructor(options: DirectLineOptions) {
         this.secret = options.secret;
         this.token = options.secret || options.token;
@@ -495,7 +497,10 @@ export class DirectLine implements IBotConnection {
         .flatMap(connectionStatus => {
             switch (connectionStatus) {
                 case ConnectionStatus.Ended:
-                    return Observable.throw(errorConversationEnded);
+                    if (this.ending)
+                        return Observable.of(connectionStatus);
+                    else
+                        return Observable.throw(errorConversationEnded);
 
                 case ConnectionStatus.FailedToConnect:
                     return Observable.throw(errorFailedToConnect);
@@ -619,13 +624,8 @@ export class DirectLine implements IBotConnection {
     end() {
         if (this.tokenRefreshSubscription)
             this.tokenRefreshSubscription.unsubscribe();
-        try {
-            this.connectionStatus$.next(ConnectionStatus.Ended);
-        } catch (e) {
-            if (e === errorConversationEnded)
-                return;
-            throw(e);
-        }
+        this.ending = true;
+        this.connectionStatus$.next(ConnectionStatus.Ended);
     }
 
     getSessionId(): Observable<string> {
