6d29c66b640a96f85f889307ed173170031ede27
William Wong <compulim@hotmail.com>
2021-07-28 16:44:46 -0700

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,27 @@ module.exports = {
   },
   externals: ['net'],
   mode: 'production',
+  module: {
+    rules: [
+      {
+        // To speed up bundling, we are limiting Babel to a number of packages which does not publish ES5 bits.
+        test: /\/node_modules\/(botframework-streaming|buffer)\//iu,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  modules: 'commonjs'
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
   output: {
     library: 'DirectLine',
     libraryTarget: 'umd'
@@ -15,5 +36,6 @@ module.exports = {
       filename: 'stats.json',
       transform: (_, opts) => JSON.stringify(opts.compiler.getStats().toJson({ chunkModules: true }), null, 2)
     })
-  ]
+  ],
+  target: ['web', 'es5']
 };
