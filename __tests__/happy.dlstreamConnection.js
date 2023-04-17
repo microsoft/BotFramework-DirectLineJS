36a2cd0ff48bbd84b7a049ded899f41b62c40286
fangyangci <fangyangci@microsoft.com>
2023-04-17 19:28:26 +0800

    fix: directLineStreaming.ts reconnect with exception

--- __tests__/happy.dlstreamConnection.js
+++ __tests__/happy.dlstreamConnection.js
@@ -5,27 +5,56 @@ import onErrorResumeNext from 'on-error-resume-next';
 import { timeouts } from './constants.json';
 import * as createDirectLine from './setup/createDirectLine';
 import waitForConnected from './setup/waitForConnected';
+import { async } from 'rxjs/scheduler/async';
+
+import {
+  ConnectionStatus
+} from '../src/directLine.ts';
 
 // TODO: Need more realistic testing.
 //       - Able to connect to a Web Socket server
 //       - Make sure after `end` is called, the client will not reconnect
 //       - If the connection is disrupted, make sure the client will reconnect
 //       - Use a fake timer to speed up the test
-
-// Skipping because the bot at dljstestbot.azurewebsites.net is not available.
-describe.skip('test dl streaming end', () => {
+describe('test dl streaming end', () => {
   let unsubscribes;
-  let directLine;
-  const ConnectionStatusEnd = 5;
+  let directLineStreaming;
   beforeEach(() => unsubscribes = []);
   afterEach(() => unsubscribes.forEach(fn => onErrorResumeNext(fn)));
   test('using Streaming Extensions', async () => {
-    jest.setTimeout(30000);
-    directLine = await createDirectLine.forStreamingExtensions();
-    unsubscribes.push(directLine.end.bind(directLine));
-    unsubscribes.push(await waitForConnected(directLine));
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    directLine.end();
-    expect(directLine.connectionStatus$.getValue()).toBe(ConnectionStatusEnd);
+    directLineStreaming = await createDirectLine.forStreamingExtensions();
+    unsubscribes.push(directLineStreaming.end.bind(directLineStreaming));
+    unsubscribes.push(await waitForConnected(directLineStreaming));
+    directLineStreaming.end();
+    expect(directLineStreaming.connectionStatus$.getValue()).toBe(ConnectionStatus.Ended);
+  })
+});
+
+describe('test dl streaming reconnect', () => {
+  let unsubscribes;
+  let directLineStreaming;
+  beforeEach(() => unsubscribes = []);
+  afterEach(() => unsubscribes.forEach(fn => onErrorResumeNext(fn)));
+  test('using Streaming Extensions', async () => {
+    let errorTimes = 2;
+
+    directLineStreaming = await createDirectLine.forStreamingExtensions();
+
+    let tmpConnect = directLineStreaming.connectAsync;
+    directLineStreaming.connectAsync = async function () {
+      errorTimes--;
+      console.log(`connectAsync errorTimes:${errorTimes}`);
+      if (errorTimes >= 0){
+        throw new Error("TestErr");
+      }
+      tmpConnect.call(directLineStreaming);
+    }
+    directLineStreaming.getRetryDelay = function () {
+      return 0;
+    }
+    unsubscribes.push(directLineStreaming.end.bind(directLineStreaming));
+    unsubscribes.push(await waitForConnected(directLineStreaming));
+
+    expect(directLineStreaming.connectionStatus$.getValue()).toBe(ConnectionStatus.Online);
   })
 });
